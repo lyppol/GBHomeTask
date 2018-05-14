1ef0bedbdc98f317aa6ce7b57216976fff276823
lyppol <p.lypovyk@google.com>
2018-05-14 17:45:43 +0000

    commit_2

--- metrics.sh
+++ metrics.sh
@@ -3,7 +3,7 @@ if [ $1 = cpu ]
 then 
 	echo system.cpu.idle    $(top -b -n1 -i | awk ' NR==3 {print $6}')
 	echo system.cpu.user    $(top -b -n1 -i | awk ' NR==3 {print $2}')
-	echo system.cpu.guest   $(top -b -n1 -i | awk ' NR==3 {print $1}')
+	echo system.cpu.guest   $(cat /proc/stat| awk ' NR==2 {print $10}')
 	echo system.cpu.iowait  $(top -b -n1 -i | awk ' NR==3 {print $10}')
 	echo system.cpu.stolen  $(top -b -n1 -i | awk ' NR==3 {print $16}')
 	echo system.cpu.system  $(top -b -n1 -i | awk ' NR==3 {print $4}')
